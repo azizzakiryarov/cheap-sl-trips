8d60b9ed067d78c9c2767cdf03d32abbcd56711c
Aziz Zakiryarov <zakiryarov@hotmail.com>
2023-04-13 15:28:53 +0200

    Added scaffolding to the project

---
+++ src/main/java/se/azza/cheapsltrips/CheapSlTripsApplication.java
@@ -0,0 +1,13 @@
+package se.azza.cheapsltrips;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheapSlTripsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheapSlTripsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/azza/cheapsltrips/CheapSlTripsApplicationTests.java
@@ -0,0 +1,13 @@
+package se.azza.cheapsltrips;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheapSlTripsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
